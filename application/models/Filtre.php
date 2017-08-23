0f0219d5160dd23bf0b025dc864edda024553fac
valero.geoffrey06@gmail.com <valero.geoffrey06@gmail.com>
2017-08-23 17:20:38 +0200

    model

--- application/models/Filtre.php
+++ application/models/Filtre.php
@@ -9,13 +9,40 @@
 class Genius_Model_Filtre
 {
     public static function getArticles() {
+
         global $db;
 
         $sql = "
 		SELECT *
 		FROM ec_filtres 
 		";
+
         $data = $db->fetchAll($sql);
+
         return $data;
+    }
+
+    public function select(){
+        global $db ;
+        
+        $sql = $db
+            ->select()
+            ->from('ec_filtres')
+            ->join('ec_products_multilingual', 'ec_filtres.product_m_id = ec_products_multilingual.id',
+                [
+                    'title','text','id_product',
+                    'carac_1','carac_2','carac_3','carac_4','carac_5','carac_6','search'
+                ])
+            ->joinLeft(
+                'ec_images_relations',
+                ' ec_products_multilingual.id_product = ec_images_relations.id_item ',['id_item']
+            )
+            ->joinLeft('ec_images', 'ec_images_relations.id_image = ec_images.id',['id_img' => 'id','filename','path_folder','format'])
+            ->where('ec_images_relations.id_module=7')
+            ->where('ec_images_relations.image_cover =1')
+        ;
+
+
+       return  $sql;
     }
 }
--- application/modules/default/controllers/FiltreController.php
+++ application/modules/default/controllers/FiltreController.php
@@ -16,29 +16,47 @@ class FiltreController extends Genius_AbstractController
         $this->view->headMeta()->appendName('description',"Contact Form");
         $this->view->headMeta()->appendName('keyword',"Easy Living | Login Form");
 
+        //Image Product
+
+        $photocover_product = Genius_Model_Product::getProductImageCoverById(16);
+        $path = (!empty($photocover_product)) ? $photocover_product['path_folder'] . '/' . $photocover_product['filename'] . '-small-' . $photocover_product['id_image'] . '.' . $photocover_product['format'] : '';
+        $photocrh_cover = THEMES_DEFAULT_URL . 'images/non_dispo.png';
+        if (file_exists(UPLOAD_PATH . 'images/' . $path) && is_file(UPLOAD_PATH . 'images/' . $path)) {
+            $photocrh_cover = UPLOAD_URL . 'images/' . $path;
+        }
+
+        //var_dump($path);
+        //var_dump($photocover_product);
+        //die();
+
         $this->view->printer = $session->printer;
         $this->view->session = $session->input;
+        $this->view->result = $session->result;
+
         $this->view->subheader = "statics/subheader.phtml";
     }
 
     public function makefiltreAction()
     {
+        // Les input choix user
         $post = $this->getRequest()->getPost();
+
+        // Instance de la session
         $session = new Zend_Session_Namespace('input');
 
+        // Instance de la classe qui vas gerer a tout filtrer et faire
+        // La recherche dans la base de donnée
         $filtering = new Genius_Class_FilteringPrinter($post);
+
         if($session->input['search'] == 'd') $filtering = new Genius_Class_FilteringDouchette($post);
         if($session->input['search'] == 't') $filtering = new Genius_Class_FilteringTerminal($post);
 
-        $filtering
+        $result = $filtering
             ->setSession($session)
             ->handle()
+            ->search()
         ;
-
-        $model = new Genius_Model_Filtre();
-        $art = $model::getArticles();
-        var_dump($model::getArticles());
-        die();
+        $session->result = $result;
 
         $baseUrl = new Zend_View_Helper_BaseUrl();
         $this->getResponse()->setRedirect($baseUrl->baseUrl().'/filtre');
@@ -59,6 +77,7 @@ class FiltreController extends Genius_AbstractController
     public function deletefiltreAction()
     {
         $session = new Zend_Session_Namespace('input');
+        unset($session->result);
 
         if ($session->input['search'] == 'p' ) unset($session->printer) ;
         elseif($session->input['search'] == 'd' )  unset($session->douchette) ;
